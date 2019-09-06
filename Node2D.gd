7ca5555c1c7d8a171a874a37d6e718cfb793185b
PLAYER-CHAI <getx1998@gmail.com>
2019-09-06 18:42:08 +0700

    add full game

--- Node2D.gd
+++ Node2D.gd
@@ -1,29 +1,24 @@
 extends Control
-var story = "Your gender is %s \nYour best skill is %s \nYour inspiration is %s \nYou are %s"
+var story = "Your in mind  is %s \nYour want to do %s \nYou want to be with %s \ndid you think %s"
 var answers = []
 var questions = []
 var questions_number = 0
 
 func _ready():
-	
-	questions.append("1. What gender of you ?")
-	questions.append("2. What is your best skill ?")
-	questions.append("3. Who is your inspiration ?")
-	questions.append("4. Are ready ?")
-	
-	$DisplayText.text = questions[0] #คำถามที่ 1
-	
-func _on_Play_Button_pressed(): #แบบแยก
+	questions.append("1. What does the goal you have in mind ?")
+	questions.append("2. What do your want to do in 10 years ?")
+	questions.append("3. The person you want to be with ?")
+	questions.append("4. What did you think of the last question ?")
+	$DisplayText.text = questions[0]
+func _on_Play_Button_pressed():
 	if questions_number < len(questions)-1: 
 		answers.append($PlayerInput.text)
 		print(answers)
 		$PlayerInput.text = ""
 		$DisplayText.text = questions[questions_number + 1]
-		questions_number = questions_number + 1 #เพิ่มค่า Questions ที่ละ 1
-
-	elif questions_number == len(questions)-1: #คำถามที่ 4
+		questions_number = questions_number + 1
+	elif questions_number == len(questions)-1:
 		answers.append($PlayerInput.text)
 		print(answers)
 		$PlayerInput.text = ""
-		
 		$DisplayText.text = story % answers
