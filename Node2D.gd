6e07295b5f09a56f10128398922200ec9f49786d
PLAYER-CHAI <getx1998@gmail.com>
2019-09-08 15:44:51 +0700

    Add a full game -EDITED-

--- Node2D.gd
+++ Node2D.gd
@@ -1,15 +1,18 @@
 extends Control
-var story = "Your in mind  is %s \nYour want to do %s \nYou want to be with %s \ndid you think %s"
+var story = "Your gender is %s \nYour best skill is %s \nYour inspiration is %s \nYou will graduate in %s"
 var answers = []
 var questions = []
 var questions_number = 0
 
 func _ready():
-	questions.append("1. What does the goal you have in mind ?")
-	questions.append("2. What do your want to do in 10 years ?")
-	questions.append("3. The person you want to be with ?")
-	questions.append("4. What did you think of the last question ?")
+	
+	questions.append("1. What gender of you ?")
+	questions.append("2. What is your best skill ?")
+	questions.append("3. Who is your inspiration ?")
+	questions.append("4. When do you graduate ?")
+	
 	$DisplayText.text = questions[0]
+	
 func _on_Play_Button_pressed():
 	if questions_number < len(questions)-1: 
 		answers.append($PlayerInput.text)
@@ -17,8 +20,10 @@ func _on_Play_Button_pressed():
 		$PlayerInput.text = ""
 		$DisplayText.text = questions[questions_number + 1]
 		questions_number = questions_number + 1
+
 	elif questions_number == len(questions)-1:
 		answers.append($PlayerInput.text)
 		print(answers)
 		$PlayerInput.text = ""
+		
 		$DisplayText.text = story % answers
